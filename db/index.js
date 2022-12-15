b33ba8e371aad67b84949d6a423023d5eced874e
Megan Miller <meganmiller485@gmail.com>
2022-12-15 12:45:49 -0700

    Part 2.2-very end

--- db/index.js
+++ db/index.js
@@ -1,30 +1,55 @@
 const {Client} = require('pg'); //this imports the pg module which we will use to link our data
-const { resourceLimits } = require('worker_threads');
+
 
 //supply the db name and location of the database
 const client = new Client('postgres://localhost:5432/juicebox-dev');
 
 //create some helper functions to use throughout the application
+async function createUser({username, password, name, location}){
+    try {
+        const {rows} = await client.query(`
+            INSERT INTO users (username, password, name, location) VALUES ($1, $2, $3, $4) 
+            ON CONFLICT (username) DO NOTHING
+            RETURNING *;
+        `, [username, password, name, location]);
+
+        return rows;
+    } catch (error) {
+        throw error;
+    };
+};
+
 
 async function getAllUsers() {
     
     //deconstruct rows to get the user objects using the query, then return those objects
     const {rows} = await client.query(
-        'SELECT id, username FROM users;'
+        'SELECT id, username, name, location, active FROM users;'
     );
 
     return rows;
 };
 
-async function createUser({username, password}){
+async function updateUser(id, fields = {}) {
+    // build the set string
+    const setString = Object.keys(fields).map(
+      (key, index) => `"${ key }"=$${ index + 1 }`
+    ).join(', ');
+  
+    // return early if this is called without fields
+    if (setString.length === 0) {
+      return;
+    };
+  
     try {
         const {rows} = await client.query(`
-            INSERT INTO users (username, password) VALUES ($1, $2) 
-            ON CONFLICT (username) DO NOTHING
-            RETURNING *;
-        `, [username, password]);
-
-        return {rows};
+          UPDATE users
+          SET ${ setString }
+          WHERE id=${ id }
+          RETURNING *;
+        `, Object.values(fields));
+    
+        return rows;
     } catch (error) {
         throw error;
     };
@@ -32,8 +57,10 @@ async function createUser({username, password}){
 
 
 
+
 module.exports = {
     client,
     getAllUsers,
     createUser,
+    updateUser,
 };
--- db/seed.js
+++ db/seed.js
@@ -1,5 +1,5 @@
 // grab our client with destructuring from the export in index.js
-const {client, getAllUsers, createUser} = require('./index');
+const {client, getAllUsers, createUser, updateUser} = require('./index');
 
 
 
@@ -31,7 +31,10 @@ async function createTables() {
             CREATE TABLE users (
                 id SERIAL PRIMARY KEY,
                 username VARCHAR(255) UNIQUE NOT NULL,
-                password VARCHAR(255) NOT NULL
+                password VARCHAR(255) NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                location VARCHAR(255) NOT NULL,
+                active BOOLEAN DEFAULT true
             );
         `);
 
@@ -47,9 +50,9 @@ async function createInitialUsers() {
     try {
         console.log("Starting to create initial users...");
 
-        const albert = await createUser({ username: 'albert', password: 'bertie99'});
-        const sandra = await createUser({ username: 'sandra', password: '2sandy4me' });
-        const glamgal = await createUser({username: 'glamgal', password: 'soglam'})
+        const albert = await createUser({ username: 'albert', password: 'bertie99', name: 'Bert', location:'Eugene, OR'});
+        const sandra = await createUser({ username: 'sandra', password: '2sandy4me', name: 'Sandy', location:'Sandy Beaches, SC' });
+        const glamgal = await createUser({username: 'glamgal', password: 'soglam', name: 'Elan', location:'Fort Collins, CO'})
         //test it!
         console.log("albert:", albert);
         console.log("sandra:", sandra);
@@ -83,9 +86,19 @@ async function testDB() {
         console.log("Starting to test database...")
         //connect the client to the database, finally
         // client.connect();
-
+        console.log("Calling getAllUsers");
         const users = await getAllUsers();
         console.log("getAllUsers:", users);
+
+
+        console.log("Calling updateUser on users[0]")
+        const updateUserResult = await updateUser(users[0].id, {
+        name: "Newname Sogood",
+        location: "Lesterville, KY"
+        });
+        console.log("Result:", updateUserResult);
+
+
 
         //queries are promises, so we can await them
         //deconstruct the rows field to give us individual id objects
